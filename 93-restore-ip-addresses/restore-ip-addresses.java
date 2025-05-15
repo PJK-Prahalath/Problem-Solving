12cfd70d1e5d94a7e5745aa6399313382a6f7898
Prahalath <125539355+PJK-Prahalath@users.noreply.github.com>
2025-05-15 20:57:27 +0530

    Time: 6 ms (35.08%) | Memory: 45.2 MB (5.61%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.java
@@ -0,0 +1,27 @@
+class Solution {
+    HashSet<String> set=new HashSet<>();
+    public List<String> restoreIpAddresses(String s) {
+        ArrayList<String> arr=new ArrayList<>();
+        dfs(0,s,arr);
+        return new ArrayList<String>(set);
+    }
+    void dfs(int i,String s,ArrayList<String> arr)
+    {
+        if(arr.size()==4 && i==s.length())
+        {
+        set.add(String.join(".",arr));
+        return;
+        }
+        for(int j=i+1;j<=Math.min(i+3,s.length());j++)
+        {
+            String str=s.substring(i,j);
+            if(Integer.parseInt(str)<=255 && (str.equals("0") || str.charAt(0)!='0'))
+            {
+                arr.add(str);
+                dfs(j,s,arr);
+                arr.remove(arr.size()-1);
+            }
+        }
+
+    }
+}
